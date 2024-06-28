e016e7cd26a018d6966dd31641d2964e6168a0bd
Valentina <vrudnitskaya@gmail.com>
2024-06-28 16:46:14 -0500

    install express, create public folder with html, edit app js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+//console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('./public'));
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: "It worked!" });
+});
+
+app.all('*', (req, res)=> {
+    res.status(404).send('404 error');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
