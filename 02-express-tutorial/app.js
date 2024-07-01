fc04d7f24c98ff9c93f2ae5c8d58bbcb5c470748
Valentina <vrudnitskaya@gmail.com>
2024-07-01 14:02:11 -0500

    add search with diff query params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -25,6 +25,17 @@ app.get('/api/v1/products/:productID', (req, res) => {
     res.json(product);
 });
 
+app.get('/api/v1/query', (req, res)=> {
+    const { search, limit, price } = req.query;
+    const queryProducts = products
+                            .filter(product => product.name.startsWith(search) && product.price < Number(price))
+                            .slice(0, limit || products.length);
+    if(queryProducts.length< 1) {
+        return res.status(200).send('No products matched your search');
+    };
+    res.status(200).json(queryProducts);
+})
+
 app.all('*', (req, res)=> {
     res.status(404).send('404 error');
 });
